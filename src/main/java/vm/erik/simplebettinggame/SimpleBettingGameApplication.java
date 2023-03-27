01d6d95ae633a6bcb41e095d82d418e63b2d9887
erik-vm <60032776+erik-vm@users.noreply.github.com>
2023-03-27 22:43:49 +0300

    initial commit

---
+++ src/main/java/vm/erik/simplebettinggame/SimpleBettingGameApplication.java
@@ -0,0 +1,13 @@
+package vm.erik.simplebettinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBettingGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBettingGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vm/erik/simplebettinggame/SimpleBettingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package vm.erik.simplebettinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBettingGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
